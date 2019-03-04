28195787a9b058fa0095b3cd8614d40d4829879f
Divad0175 <dgranjon@ymail.com>
2019-03-04 11:42:40 +0100

    fix travis

--- R/useTemplate.R
+++ R/useTemplate.R
@@ -1,10 +1,10 @@
 #' Use HTML templates in 'shiny'
 #'
-#' Allow to use functions from 'shinydashboard', 'shinydashboardPlus', 'bs4Dash', 'shinymaterial' into a classic 'shiny' app,
+#' Allow to use functions from 'shinydashboard', 'shinydashboardPlus', 'bs4Dash' into a classic 'shiny' app,
 #' without the need to use sidebars, navbars or any sofisticated layout. The idea is
 #' to be as simple as possible.
 #'
-#' @param lib Choose a flavor among \code{c("shinydashboard", "shinydashboardPlus", "bs4Dash", "shinymaterialPlus")}.
+#' @param lib Choose a flavor among \code{c("shinydashboard", "shinydashboardPlus", "bs4Dash")}.
 #'
 #' @export
 #'
@@ -407,183 +407,8 @@
 #'
 #' }
 #'
-#' # shinymaterial
-#' #' if (interactive()) {
-#'
-#' library(shiny)
-#' library(shinyWidgets)
-#' library(shinymaterial)
-#' library(shinymaterialPlus)
-#'
-#' ui <- fluidPage(
-#'   title = "Material design",
-#'   useTemplate(lib = "shinymaterialPlus"),
-#'   h1("Import materialize CSS inside shiny!", align = "center"),
-#'   h5("Don't need any sidebar, navbar, ...", align = "center"),
-#'   h5("Only focus on basic elements for a pure interface", align = "center"),
-#'   hr(),
-#'   fluidRow(
-#'     column(
-#'       width = 3,
-#'       shinymaterialPlus::material_card(
-#'         title = "Example Card",
-#'         depth = 5,
-#'         hoverable = FALSE,
-#'         size = "large",
-#'         tabs = TRUE,
-#'         tabscontent = material_tabs(
-#'           tabs = c(
-#'             "Example Tab 1" = "example_tab_1",
-#'             "Example Tab 2" = "example_tab_2"
-#'           ),
-#'           color = "deep-purple"
-#'         ),
-#'         material_card_tabs(
-#'           id = "example_tab_1",
-#'           "Tab 1"
-#'         ),
-#'         material_card_tabs(id = "example_tab_2", "Tab 2"),
-#'         activator = TRUE,
-#'         extra = tagList(
-#'           "More info here!",
-#'           material_badge(1, custom_caption = TRUE),
-#'           material_badge(content = "Test", color = "red", type = "new")
-#'         ),
-#'         #color = "deep-orange",
-#'         image = TRUE,
-#'         src = "http://www.oxygenna.com/wp-content/uploads/2015/11/18.jpg"
-#'       )
-#'     ),
-#'     column(
-#'       width = 3,
-#'       shinymaterialPlus::material_card(
-#'         title = "Card with footer",
-#'         depth = NULL,
-#'         size = "large",
-#'         activator = TRUE,
-#'         extra = "More info here!",
-#'         image = TRUE,
-#'         src = "https://i.pinimg.com/originals/73/38/6e/73386e0513d4c02a4fbb814cadfba655.jpg",
-#'         footer = tagList(
-#'           material_row(
-#'             material_column(
-#'               s_width = 6,
-#'               shiny::a(href = "http://www.google.fr", "Link 1")
-#'             ),
-#'             material_column(
-#'               s_width = 6,
-#'               shiny::a(href = "http://www.google.fr", "Link 2")
-#'             )
-#'           )
-#'         )
-#'       )
-#'     ),
-#'     column(
-#'       width = 4,
-#'       material_collapsible(
-#'         type = "popout",
-#'         material_collapsible_item(
-#'           "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-#'               sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-#'               Ut enim ad minim veniam, quis nostrud exercitation ullamco
-#'               laboris nisi ut aliquip ex ea commodo consequat",
-#'           header = "First",
-#'           active = TRUE,
-#'           icon = "filter_drama"
-#'         ),
-#'         material_collapsible_item(
-#'           "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-#'               sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-#'               Ut enim ad minim veniam, quis nostrud exercitation ullamco
-#'               laboris nisi ut aliquip ex ea commodo consequat",
-#'           header = "Second",
-#'           icon = "place"
-#'         ),
-#'         material_collapsible_item(
-#'           "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-#'               sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-#'               Ut enim ad minim veniam, quis nostrud exercitation ullamco
-#'               laboris nisi ut aliquip ex ea commodo consequat",
-#'           header = "Third",
-#'           icon = "whatshot"
-#'         )
-#'       )
-#'     )
-#'   ),
-#'   fluidRow(
-#'     column(
-#'       width = 6,
-#'       material_timeline(
-#'         material_timeline_item(
-#'           material_timeline_card(
-#'             src = "https://i.pinimg.com/originals/df/0a/3e/df0a3e2ec30abb1c92d145ef165b714f.gif",
-#'             title = "Item 1",
-#'             extra = "Some text here!"
-#'           ),
-#'           marker_icon = "language",
-#'           marker_color = "blue",
-#'           marker_size = "large"
-#'         ),
-#'         material_timeline_item(
-#'           material_timeline_card(
-#'             src = "https://kulturologia.ru/files/u12645/Rhads-2.jpg",
-#'             hoverable = TRUE,
-#'             title = "Item 2",
-#'             extra = "Some text here!"
-#'           ),
-#'           marker_icon = "work",
-#'           marker_color = "green",
-#'           marker_size = "small"
-#'         )
-#'       )
-#'     ),
-#'     column(
-#'       width = 6,
-#'       material_column(
-#'         shinymaterialPlus::material_button(
-#'           input_id = "example_button",
-#'           label = "Button",
-#'           icon = "cloud",
-#'           depth = 5,
-#'           color = "blue lighten-2",
-#'           pulse = TRUE
-#'         ),
-#'         shinymaterialPlus::material_button(
-#'           input_id = "example_button2",
-#'           icon = "cloud",
-#'           label = "",
-#'           depth = 3,
-#'           color = "green lighten-2",
-#'           pulse = TRUE,
-#'           floating = TRUE
-#'         ),
-#'         shinymaterialPlus::material_button(
-#'           input_id = "example_button3",
-#'           icon = "cloud",
-#'           label = "large button",
-#'           depth = 3,
-#'           color = "orange lighten-2",
-#'           size = "large"
-#'         ),
-#'         shinymaterialPlus::material_button(
-#'           input_id = "example_button3",
-#'           icon = "cloud",
-#'           label = "Small disabled button",
-#'           depth = 3,
-#'           color = "orange lighten-2",
-#'           size = "small",
-#'           disabled = TRUE
-#'         )
-#'       )
-#'     )
-#'   )
-#'   )
-#'   shinyApp(ui, server = function(input, output) {})
-#'
 #' }
-#'
-#' }
-useTemplate <- function(lib = c("shinydashboard", "shinydashboardPlus", "bs4Dash", "shinymaterialPlus")) {
+useTemplate <- function(lib = c("shinydashboard", "shinydashboardPlus", "bs4Dash")) {
   lib <- match.arg(lib)
 
   if (!requireNamespace(package = lib))
@@ -607,8 +432,6 @@ useTemplate <- function(lib = c("shinydashboard", "shinydashboardPlus", "bs4Dash
       sidebar = bs4Dash::bs4DashSidebar(),
       body = bs4Dash::bs4DashBody()
     ))
-  } else if (lib == "shinymaterialPlus") {
-    findDependencies(shinymaterialPlus::material_page())
   }
 
   if (lib == "shinydashboard") {
@@ -618,15 +441,3 @@ useTemplate <- function(lib = c("shinydashboard", "shinydashboardPlus", "bs4Dash
   }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
