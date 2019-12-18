cb0cc2b68b9e9319e768db2537233f62a69b0fb0
pvictor <perrier.victor@gmail.com>
2019-12-18 16:54:36 +0100

    attempt to select multiple value in pickerGroup

--- R/module-pickerGroup.R
+++ R/module-pickerGroup.R
@@ -20,99 +20,7 @@
 #' @importFrom shiny NS actionLink icon
 #' @importFrom utils modifyList
 #'
-#' @examples
-#' if (interactive()) {
-#'
-#' library(shiny)
-#' library(shinyWidgets)
-#'
-#'
-#' data("mpg", package = "ggplot2")
-#'
-#'
-#' ui <- fluidPage(
-#'   fluidRow(
-#'     column(
-#'       width = 10, offset = 1,
-#'       tags$h3("Filter data with picker group"),
-#'       panel(
-#'         pickerGroupUI(
-#'           id = "my-filters",
-#'           params = list(
-#'             manufacturer = list(inputId = "manufacturer", label = "Manufacturer:"),
-#'             model = list(inputId = "model", label = "Model:"),
-#'             trans = list(inputId = "trans", label = "Trans:"),
-#'             class = list(inputId = "class", label = "Class:")
-#'           )
-#'         ), status = "primary"
-#'       ),
-#'       DT::dataTableOutput(outputId = "table")
-#'     )
-#'   )
-#' )
-#'
-#' server <- function(input, output, session) {
-#'   res_mod <- callModule(
-#'     module = pickerGroupServer,
-#'     id = "my-filters",
-#'     data = mpg,
-#'     vars = c("manufacturer", "model", "trans", "class")
-#'   )
-#'   output$table <- DT::renderDataTable(res_mod())
-#' }
-#'
-#' shinyApp(ui, server)
-#'
-#' }
-#'
-#'
-#' ### Not inline example
-#'
-#' if (interactive()) {
-#'
-#'   library(shiny)
-#'   library(shinyWidgets)
-#'
-#'
-#'   data("mpg", package = "ggplot2")
-#'
-#'
-#'   ui <- fluidPage(
-#'     fluidRow(
-#'       column(
-#'         width = 4,
-#'         tags$h3("Filter data with picker group"),
-#'         pickerGroupUI(
-#'           id = "my-filters",
-#'           inline = FALSE,
-#'           params = list(
-#'             manufacturer = list(inputId = "manufacturer", label = "Manufacturer:"),
-#'             model = list(inputId = "model", label = "Model:"),
-#'             trans = list(inputId = "trans", label = "Trans:"),
-#'             class = list(inputId = "class", label = "Class:")
-#'           )
-#'         )
-#'       ),
-#'       column(
-#'         width = 8,
-#'         DT::dataTableOutput(outputId = "table")
-#'       )
-#'     )
-#'   )
-#'
-#'   server <- function(input, output, session) {
-#'     res_mod <- callModule(
-#'       module = pickerGroupServer,
-#'       id = "my-filters",
-#'       data = mpg,
-#'       vars = c("manufacturer", "model", "trans", "class")
-#'     )
-#'     output$table <- DT::renderDataTable(res_mod())
-#'   }
-#'
-#'   shinyApp(ui, server)
-#'
-#' }
+#' @example examples/pickerGroup.R
 pickerGroupUI <- function(id, params, label = NULL, btn_label = "Reset filters", options = list(), inline = TRUE) {
 
   # Namespace
@@ -195,19 +103,18 @@ pickerGroupUI <- function(id, params, label = NULL, btn_label = "Reset filters",
 
 }
 
-#' @param input standard \code{shiny} input.
-#' @param output standard \code{shiny} output.
-#' @param session standard \code{shiny} session.
+#' @param input,output,session Standards \code{shiny} server arguments.
 #' @param data a \code{data.frame}, or an object that can be coerced to \code{data.frame}.
 #' @param vars character, columns to use to create filters,
 #'  must correspond to variables listed in \code{params}.
+#' @param multiple If \code{TRUE} allow to select multiple choices in menus.
 #'
 #' @export
 #'
 #' @rdname pickerGroup-module
 #' @importFrom shiny observeEvent reactiveValues reactive observe reactiveValuesToList
 #' @importFrom stats aggregate as.formula
-pickerGroupServer <- function(input, output, session, data, vars) { # nocov start
+pickerGroupServer <- function(input, output, session, data, vars, multiple = FALSE) { # nocov start
 
   data <- as.data.frame(data)
 
@@ -242,46 +149,102 @@ pickerGroupServer <- function(input, output, session, data, vars) { # nocov star
 
   data_r <- reactiveValues()
 
-  observe({
-    inputs <- reactiveValuesToList(input)
-    inputs[["reset_all"]] <- NULL
-    indicator <- lapply(
-      X = vars,
-      FUN = function(x) {
-        data[[x]] %inT% inputs[[x]]
-      }
-    )
-    data$indicator <- Reduce(f = `&`, x = indicator)
-    if (all(data$indicator)) {
-      toggleDisplayServer(session = session, id = ns("reset_all"), display = "none")
-    } else {
-      toggleDisplayServer(session = session, id = ns("reset_all"), display = "block")
-    }
+  if (isTRUE(multiple)) {
     lapply(
       X = vars,
       FUN = function(x) {
-        tmp <- aggregate(
-          formula = as.formula(paste("indicator", x, sep = "~")),
-          data = data,
-          FUN = Reduce, f = `|`
-        )
-        updatePickerInput(
-          session = session,
-          inputId = x,
-          choices = tmp[[x]],
-          selected = inputs[[x]],
-          choicesOpt = list(
-            disabled = !tmp$indicator,
-            style = ifelse(
-              !tmp$indicator,
-              yes = "color: rgba(119, 119, 119, 0.5);",
-              no = ""
-            )
+        observeEvent(input[[x]], {
+          others <- setdiff(vars, x)
+          inputs <- lapply(
+            X = setNames(vars, vars),
+            FUN = function(y) {
+              input[[y]]
+            }
           )
-        )
+          inputs[["reset_all"]] <- NULL
+          indicator <- lapply(
+            X = vars,
+            FUN = function(x) {
+              data[[x]] %inT% inputs[[x]]
+            }
+          )
+          data$indicator <- Reduce(f = `&`, x = indicator)
+          if (all(data$indicator)) {
+            toggleDisplayServer(session = session, id = ns("reset_all"), display = "none")
+          } else {
+            toggleDisplayServer(session = session, id = ns("reset_all"), display = "block")
+          }
+          lapply(
+            X = others,
+            FUN = function(other) {
+              tmp <- aggregate(
+                formula = as.formula(paste("indicator", other, sep = "~")),
+                data = data,
+                FUN = Reduce, f = `|`
+              )
+              tmp$indicator[tmp[[other]] %in% input[[other]]] <- TRUE
+              updatePickerInput(
+                session = session,
+                inputId = other,
+                choices = tmp[[other]],
+                selected = input[[other]],
+                choicesOpt = list(
+                  disabled = !tmp$indicator,
+                  style = ifelse(
+                    !tmp$indicator,
+                    yes = "color: rgba(119, 119, 119, 0.5);",
+                    no = ""
+                  )
+                )
+              )
+            }
+          )
+        }, ignoreNULL = FALSE)
       }
     )
-  })
+  } else {
+    observe({
+      inputs <- reactiveValuesToList(input)
+      inputs[["reset_all"]] <- NULL
+      indicator <- lapply(
+        X = vars,
+        FUN = function(x) {
+          data[[x]] %inT% inputs[[x]]
+        }
+      )
+      data$indicator <- Reduce(f = `&`, x = indicator)
+      if (all(data$indicator)) {
+        toggleDisplayServer(session = session, id = ns("reset_all"), display = "none")
+      } else {
+        toggleDisplayServer(session = session, id = ns("reset_all"), display = "block")
+      }
+      lapply(
+        X = vars,
+        FUN = function(x) {
+          tmp <- aggregate(
+            formula = as.formula(paste("indicator", x, sep = "~")),
+            data = data,
+            FUN = Reduce, f = `|`
+          )
+          updatePickerInput(
+            session = session,
+            inputId = x,
+            choices = tmp[[x]],
+            selected = inputs[[x]],
+            choicesOpt = list(
+              disabled = !tmp$indicator,
+              style = ifelse(
+                !tmp$indicator,
+                yes = "color: rgba(119, 119, 119, 0.5);",
+                no = ""
+              )
+            )
+          )
+        }
+      )
+    })
+  }
+
 
 
   return(reactive({
