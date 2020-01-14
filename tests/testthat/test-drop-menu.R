f7d0c211e69dfe18666cfcf473b5cfc052371633
pvictor <perrier.victor@gmail.com>
2020-01-14 17:19:31 +0100

    tests for drop-menu

---
+++ tests/testthat/test-drop-menu.R
@@ -0,0 +1,27 @@
+context("drop-menu")
+
+test_that("dropMenu works", {
+
+  tag <- dropMenu(
+    shiny::actionButton("go0", "See what"),
+    shiny::tags$div(
+      "Content"
+    ),
+    theme = "light-border",
+    placement = "right",
+    arrow = FALSE
+  )
+  deps <- htmltools::findDependencies(tag)
+
+  expect_is(tag, "shiny.tag")
+  expect_true(length(deps) > 0)
+})
+
+
+test_that("dropMenuOptions works", {
+
+  opts <- dropMenuOptions(duration = 500, custom = TRUE)
+
+  expect_is(opts, "list")
+  expect_identical(opts$custom, TRUE)
+})
