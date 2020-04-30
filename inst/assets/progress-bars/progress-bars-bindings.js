42d5f162165cb266b5dba77dea63f289c59992d6
pvictor <perrier.victor@gmail.com>
2020-04-30 18:48:11 +0200

    updated progressbar bindings

--- inst/assets/progress-bars/progress-bars-bindings.js
+++ inst/assets/progress-bars/progress-bars-bindings.js
@@ -1,34 +1,36 @@
-
-
 // ProgressBars bindings
-Shiny.addCustomMessageHandler('update-progressBar-shinyWidgets', function(data) {
+Shiny.addCustomMessageHandler("update-progressBar-shinyWidgets", function(
+  data
+) {
   var id = data.id;
+  var el = document.getElementById(id);
+  var elVal = document.getElementById(id + "-value");
+  var elTot = document.getElementById(id + "-total");
+  var elTitle = document.getElementById(id + "-title");
   var total = data.total;
   var value = Math.round(data.value);
-  //console.log(data.statusp);
-  //var statusp = typeof data.statusp !== 'undefined' ? data.statusp : "none";
   var pct;
   if (total > 0) {
-    pct = Math.round(value / total * 100);
-    $('#' + id + '-value').text(value);
-    $('#' + id + '-total').text(total);
-    value = Math.round(value / total * 100);
+    pct = Math.round((value / total) * 100);
+    elVal.innerText = value;
+    elTot.innerText = total;
+    value = Math.round((value / total) * 100);
   } else {
     pct = data.percent > 0 ? data.percent : value;
   }
-  $('#' + id).css('width', pct + '%');
-  var txt = $('#' + id).text();
-  if (txt !== "") { //value.display_pct !== undefined
-    $('#' + id).text(value + data.unit_mark);
+  el.style.width = pct + "%";
+  var txt = el.innerText;
+  if (txt !== "") {
+    //value.display_pct !== undefined
+    el.innerText = value + data.unit_mark;
   }
   if (data.status !== null) {
-    $('#' + id).removeClass();
-    $('#' + id).addClass("progress-bar progress-bar-" + data.status);
+    el.className = "";
+    el.classList.add("progress-bar");
+    el.classList.add("progress-bar-" + data.status);
   }
   if (data.title !== null) {
-    $('#' + id + '-title').text(data.title);
+    elTitle.innerText = data.title;
   }
 });
-
-
 
