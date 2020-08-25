b27b691726a89d9ed3050d18cf98e42f6d4703f0
pvictor <perrier.victor@gmail.com>
2020-08-25 14:20:17 +0200

    correct namespace

--- inst/assets/air-datepicker2/i18n/datepicker.ja.js
+++ inst/assets/air-datepicker2/i18n/datepicker.ja.js
@@ -1,4 +1,4 @@
-;(function ($) { $.fn.datepicker.language['ja'] = {
+;(function ($) { $.fn.airdatepicker.language['ja'] = {
     days: ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日'],
     daysShort: ['日曜', '月曜', '火曜', '水曜', '木曜', '金曜', '土曜'],
     daysMin: ['日', '月', '火', '水', '木', '金', '土'],
