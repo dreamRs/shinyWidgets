075112a024d33d9c9d9e9ababf8461fbcbde3ac9
pvictor <perrier.victor@gmail.com>
2021-01-12 12:00:53 +0100

    tests gallery

--- tests/testthat/test-shinyWidgetsGallery.R
+++ tests/testthat/test-shinyWidgetsGallery.R
@@ -5,7 +5,13 @@ context("shinyWidgetsGallery")
 test_that("Default", {
 
   library("shiny")
-  source(file = system.file('examples/shinyWidgets/global.R', package='shinyWidgets'))
-  source(file = system.file('examples/shinyWidgets/ui.R', package='shinyWidgets'))
+  expect_message(
+    source(file = system.file('examples/shinyWidgets/global.R', package='shinyWidgets'))
+  )
+
+  expect_silent(
+    ui <- source(file = system.file('examples/shinyWidgets/ui.R', package='shinyWidgets'))
+  )
+  expect_is(ui$value, "shiny.tag")
 
 })
