b9d67073b3d43b6cc6ba22307c2f54c83686f85f
InfinityLoop <swsoyee@gmail.com>
2021-05-16 01:11:42 +0900

    feat: add commas in value and total of progressBar

--- inst/assets/progress-bars/progress-bars-bindings.js
+++ inst/assets/progress-bars/progress-bars-bindings.js
@@ -13,9 +13,9 @@ Shiny.addCustomMessageHandler("update-progressBar-shinyWidgets", function(
   if (total > 0) {
     pct = Math.round((value / total) * 100);
     if (elVal !== null)
-      elVal.innerText = value;
+      elVal.innerText = data.commas ? value.toLocaleString("en-US") : value;
     if (elTot !== null)
-      elTot.innerText = total;
+      elTot.innerText = data.commas ? total.toLocaleString("en-US") : total;
     value = Math.round((value / total) * 100);
   } else {
     pct = data.percent > 0 ? data.percent : value;
