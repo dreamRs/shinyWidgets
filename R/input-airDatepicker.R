68a8d439daa235680361f91da1a866d9aa1c7ccd
Víctor Pastor López <victor.pastor@seidor.com>
2022-11-22 19:59:04 +0100

    Add Catalan language to airDatepicker

--- R/input-airDatepicker.R
+++ R/input-airDatepicker.R
@@ -138,9 +138,9 @@ airDatepickerInput <- function(inputId,
   # dput(tools::file_path_sans_ext(list.files("node_modules/air-datepicker/locale/", pattern = "\\.js")))
   language <- match.arg(
     arg = language,
-    choices = c("ar", "cs", "da", "de", "en", "es", "fi", "fr", "hu", "it",
-                "nl", "pl", "pt-BR", "pt", "ro", "ru", "si", "sk", "sv", "th",
-                "tr", "uk", "zh", "ja", "ko"),
+    choices = c("ar", "ca", "cs", "da", "de", "en", "es", "fi", "fr", "hu",
+                "it", "nl", "pl", "pt-BR", "pt", "ro", "ru", "si", "sk", "sv",
+                 "th", "tr", "uk", "zh", "ja", "ko"),
     several.ok = FALSE
   )
 
---
+++ inst/assets/air-datepicker/i18n/datepicker.ca.js
@@ -0,0 +1,12 @@
+;(function ($) { $.fn.datepicker.language['ca'] = {
+    days: ['Diumenge', 'Dilluns', 'Dimarts', 'Dimecres', 'Dijous', 'Divendres', 'Dissabte'],
+    daysShort: ['Diu', 'Dill', 'Dim', 'Dim', 'Dij', 'Div', 'Dis'],
+    daysMin: ['Dg', 'Dl', 'Dt', 'Dc', 'Dj', 'Dv', 'Ds'],
+    months: ['Gener','Febrer','Març','Abril','Maig','Juny', 'Juliol','Agost','Setembre','Octubre','Novembre','Desembre'],
+    monthsShort: ['Gen', 'Feb', 'Març', 'Abr', 'Maig', 'Juny', 'Jul', 'Ag', 'Set', 'Oct', 'Nov', 'Des'],
+    today: 'Avui',
+    clear: 'Netejar',
+    dateFormat: 'dd/mm/yyyy',
+    timeFormat: 'hh:ii aa',
+    firstDay: 1
+}; })(jQuery);
