5ae7f64d4efa6b5e517578aa83a036c1e25d7c31
mkranjcev <kranjcev@srce.hr>
2023-06-16 12:57:06 +0200

    DP-addon now a CSS class, instead of inline style
    Specified multiple appropriate classes in CSS to avoid using !important

--- R/input-airDatepicker.R
+++ R/input-airDatepicker.R
@@ -215,8 +215,7 @@ airDatepickerInput <- function(inputId,
         class = "input-group",
         if (identical(addon, "left")) {
           tags$div(
-            style = "display: table-cell;",
-            class = "btn action-button input-group-addon",
+            class = "btn action-button input-group-addon dp-addon",
             id = paste0(inputId, "_button"),
             icon("calendar-days")
           )
@@ -224,8 +223,7 @@ airDatepickerInput <- function(inputId,
         tagAir,
         if (identical(addon, "right")) {
           tags$div(
-            style = "display: table-cell;",
-            class = "btn action-button input-group-addon",
+            class = "btn action-button input-group-addon dp-addon",
             id = paste0(inputId, "_button"),
             icon("calendar-days")
           )
