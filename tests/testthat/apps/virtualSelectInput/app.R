e80f21121969d79cb981b6de764924bb3a993ffa
Jon Harmon <jon.harmon@atorusresearch.com>
2025-06-26 09:30:20 -0500

    Test virtualSelectInput with shinytest2
    I pinned shiny to v1.10.0 until they push 1.11.1. This test fails with 1.11.0 (which is most of the point of this test!).

---
+++ tests/testthat/apps/virtualSelectInput/app.R
@@ -0,0 +1,73 @@
+library(shiny)
+
+# Use built package for R-CMD-check-like checks, local for other testing.
+if (
+  isTRUE(as.logical(Sys.getenv("TESTTHAT_IS_CHECKING", "false"))) ||
+  isTRUE(as.logical(Sys.getenv("R_COVR", "false")))
+) {
+  library("shinyWidgets")
+} else {
+  pkgload::load_all(here::here())
+}
+
+ui <- fluidPage(
+  tags$h2("Virtual Select"),
+
+  fluidRow(
+    column(
+      width = 4,
+      virtualSelectInput(
+        inputId = "single",
+        label = "Single select :",
+        choices = month.name
+      ),
+      virtualSelectInput(
+        inputId = "multiple",
+        label = "Multiple select:",
+        choices = setNames(month.abb, month.name),
+        multiple = TRUE
+      ),
+      virtualSelectInput(
+        inputId = "onclose",
+        label = "Update value on close:",
+        choices = setNames(month.abb, month.name),
+        multiple = TRUE,
+        updateOn = "close"
+      )
+    ),
+    column(
+      width = 4,
+      tags$b("Single select :"),
+      verbatimTextOutput("res_single"),
+      tags$b("Is virtual select open?"),
+      verbatimTextOutput(outputId = "res_single_open"),
+
+      tags$br(),
+
+      tags$b("Multiple select :"),
+      verbatimTextOutput("res_multiple"),
+      tags$b("Is virtual select open ?"),
+      verbatimTextOutput(outputId = "res_multiple_open"),
+
+      tags$br(),
+
+      tags$b("Update on close :"),
+      verbatimTextOutput("res_onclose"),
+      tags$b("Is virtual select open ?"),
+      verbatimTextOutput(outputId = "res_onclose_open")
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  output$res_single <- renderPrint(input$single)
+  output$res_single_open <- renderPrint(input$single_open)
+
+  output$res_multiple <- renderPrint(input$multiple)
+  output$res_multiple_open <- renderPrint(input$multiple_open)
+
+  output$res_onclose <- renderPrint(input$onclose)
+  output$res_onclose_open <- renderPrint(input$onclose_open)
+}
+
+shinyApp(ui, server)
